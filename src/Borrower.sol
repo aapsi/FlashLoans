f0bc9e7af6d12ea5d02f90405bdba83557afe7a0
aapsi <aapsikhaira98@gmail.com>
2025-06-08 04:01:04 +0530

    Implement LoanBorrower contract with flash loan functionality and necessary imports

--- src/Borrower.sol
+++ src/Borrower.sol
@@ -1,6 +1,29 @@
 // SPDX-License-Identifier: MIT
 pragma solidity 0.8.30;
 
-contract LoanBorrower {
-    
+import {IERC3156FlashBorrower} from "@openzeppelin/contracts/interfaces/IERC3156FlashBorrower.sol";
+import {IERC20} from "@openzeppelin/contracts/token/ERC20/IERC20.sol";
+import {SafeERC20} from "@openzeppelin/contracts/token/ERC20/utils/SafeERC20.sol";
+import {ReentrancyGuard} from "@openzeppelin/contracts/utils/ReentrancyGuard.sol";
+import {Ownable} from "@openzeppelin/contracts/access/Ownable.sol";
+contract LoanBorrower is IERC3156FlashBorrower, ReentrancyGuard, Ownable {
+    using SafeERC20 for IERC20;
+
+    IERC20 public token;
+    address public initiator;
+
+    constructor(address _token, address _initiator) Ownable(msg.sender) {
+        token = IERC20(_token);
+        initiator = _initiator;
+    }
+
+    function onFlashLoan(
+        address _initiator,
+        address _token,
+        uint256 _amount,
+        uint256 _fee,
+        bytes calldata data
+    ) external override returns (bytes32) {
+        // TODO: Implement flash loan logic
+    }
 }
