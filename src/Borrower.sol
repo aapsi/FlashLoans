639e7a2140d342a36aaeeae2e44d8129e1b974e9
aapsi <aapsikhaira98@gmail.com>
2025-06-11 09:20:50 +0530

    Refactor LoanBorrower and FlashLoan contracts to enhance flash loan functionality and add deposit/withdraw features. Introduced error handling and events for better state management. Updated constructor parameters and added interface for arbitrage operations.

--- src/Borrower.sol
+++ src/Borrower.sol
@@ -2,19 +2,50 @@
 pragma solidity 0.8.30;
 
 import {IERC3156FlashBorrower} from "@openzeppelin/contracts/interfaces/IERC3156FlashBorrower.sol";
+import {IERC3156FlashLender} from "@openzeppelin/contracts/interfaces/IERC3156FlashLender.sol";
 import {IERC20} from "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 import {SafeERC20} from "@openzeppelin/contracts/token/ERC20/utils/SafeERC20.sol";
 import {ReentrancyGuard} from "@openzeppelin/contracts/utils/ReentrancyGuard.sol";
-import {Ownable} from "@openzeppelin/contracts/access/Ownable.sol";
-contract LoanBorrower is IERC3156FlashBorrower, ReentrancyGuard, Ownable {
+
+interface IArbitrage {
+    function arbitrage(address _token, uint256 _amount) external returns (bool);
+}
+
+contract LoanBorrower is IERC3156FlashBorrower, ReentrancyGuard{
     using SafeERC20 for IERC20;
 
     IERC20 public token;
     address public initiator;
+    IERC3156FlashLender public flashLoan;
+    address public arbitrage;
 
-    constructor(address _token, address _initiator) Ownable(msg.sender) {
+
+    mapping(address => uint256) public loanAmount;
+
+
+    error InvalidAddress();
+    error FlashLoanFailed();
+    error InvalidLender();
+    error InvalidInitiator();
+    error ArbitrageFailed();
+    
+    event FlashLoanBorrowed(address indexed initiator, uint256 amount);
+
+    constructor(address _token, address _initiator, IERC3156FlashLender _flashLoan, address _arbitrage) {
+        if(_token ==address(0) || _initiator == address(0) || address(_flashLoan) == address(0)) revert InvalidAddress();
         token = IERC20(_token);
         initiator = _initiator;
+        flashLoan = _flashLoan;
+        arbitrage = _arbitrage;
+    }
+
+    function borrow(uint256 amount) external nonReentrant returns (bool) {
+        loanAmount[msg.sender] = amount;
+        bool success = flashLoan.flashLoan(this, address(token), amount, "");
+        if(!success) revert FlashLoanFailed();
+
+        emit FlashLoanBorrowed(msg.sender, amount);
+        return success;
     }
 
     function onFlashLoan(
@@ -22,8 +53,17 @@ contract LoanBorrower is IERC3156FlashBorrower, ReentrancyGuard, Ownable {
         address _token,
         uint256 _amount,
         uint256 _fee,
-        bytes calldata data
-    ) external override returns (bytes32) {
-        // TODO: Implement flash loan logic
+        bytes calldata
+    ) external override nonReentrant returns (bytes32) {
+        if(msg.sender != address(flashLoan)) revert InvalidLender();
+        if(_initiator != address(this)) revert InvalidInitiator();
+
+       // TODO: flashloan logic : swap, arbitrage, etc
+       bool success = IArbitrage(arbitrage).arbitrage(_token, _amount);
+       if(!success) revert ArbitrageFailed();
+
+       // aderyn-ignore-next-line(unchecked-return)
+       IERC20(_token).safeIncreaseAllowance(address(flashLoan), _amount + _fee);
+       return keccak256("ERC3156FlashBorrower.onFlashLoan");
     }
 }
--- src/FlashLoan.sol
+++ src/FlashLoan.sol
@@ -4,42 +4,72 @@ pragma solidity 0.8.30;
 import {IERC20} from "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 import {SafeERC20} from "@openzeppelin/contracts/token/ERC20/utils/SafeERC20.sol";
 import {ReentrancyGuard} from "@openzeppelin/contracts/utils/ReentrancyGuard.sol";
-import {Ownable} from "@openzeppelin/contracts/access/Ownable.sol";
 import {IERC3156FlashLender} from "@openzeppelin/contracts/interfaces/IERC3156FlashLender.sol";
 import {IERC3156FlashBorrower} from "@openzeppelin/contracts/interfaces/IERC3156FlashBorrower.sol";
 
-contract FlashLoan is Ownable, ReentrancyGuard, IERC3156FlashLender {
+contract FlashLoan is ReentrancyGuard, IERC3156FlashLender {
     using SafeERC20 for IERC20;
-
-    uint256 public constant MAX_LOAN_AMOUNT = 1000000000000000000000000;
-    uint256 public constant FLASH_LOAN_FEE = 1000000000000000000;
 
     IERC20 public token;
 
-    constructor(address _token) Ownable(msg.sender) {
+    uint256 public constant FLASH_LOAN_FEE = 5; // 0.05%
+
+    mapping(address => uint256) public depositedAmount;
+
+    error InvalidToken();
+    error InsufficientBalance();
+    error FlashLoanFailed();
+    error RepaymentFailed();
+
+    event Deposit(address indexed user, uint256 amount);
+    event Withdraw(address indexed user, uint256 amount);
+
+    constructor(address _token) {
+        if (_token == address(0)) revert InvalidToken();
         token = IERC20(_token);
-        
     }
 
-    function maxFlashLoan(address _token) external pure returns (uint256) {
-        return MAX_LOAN_AMOUNT;
+    function deposit(uint256 _amount) external {
+        token.safeTransferFrom(msg.sender, address(this), _amount);
+        depositedAmount[msg.sender] += _amount;
+        emit Deposit(msg.sender, _amount);
     }
 
-    function flashFee(address _token, uint256 _amount) external pure returns (uint256) {
-        return FLASH_LOAN_FEE;
+    function maxFlashLoan(address _token) external view returns (uint256) {
+        return IERC20(_token).balanceOf(address(this));
+    }
+    function flashFee(
+        address _token,
+        uint256 _amount
+    ) external pure override returns (uint256) {
+        return (_amount * FLASH_LOAN_FEE) / 10_000;
     }
 
-
-    function flashLoan(        
+    function flashLoan(
         IERC3156FlashBorrower receiver,
         address _token,
         uint256 _amount,
-        bytes calldata data) external override returns (bool) {
-        // TODO: Implement flash loan logic
+        bytes calldata data
+    ) external override nonReentrant returns (bool) {
+        if (_token != address(token)) revert InvalidToken();
+        if (_amount > token.balanceOf(address(this)))
+            revert InsufficientBalance();
+        address initiator = msg.sender;
+
+        token.safeTransfer(address(receiver), _amount);
+        if (
+            receiver.onFlashLoan(initiator, _token, _amount, 0, data) !=
+            keccak256("ERC3156FlashBorrower.onFlashLoan")
+        ) revert FlashLoanFailed();
+
+        if (!token.transferFrom(address(receiver), address(this), _amount))
+            revert RepaymentFailed();
+        return true;
     }
 
-    function withdraw() public {
-        // TODO: Implement safe withdraw logic
-
+    function withdraw() external nonReentrant {
+        token.safeTransfer(msg.sender, depositedAmount[msg.sender]);
+        depositedAmount[msg.sender] = 0;
+        emit Withdraw(msg.sender, depositedAmount[msg.sender]);
     }
 }
